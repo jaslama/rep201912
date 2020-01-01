94025e09d21e20a94a81933ebf06cd9c612b5939
jasla <jasla@LAPTOP-20AA7S94>
2020-01-01 22:03:38 +0100

    xxx

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
 
 public class Main {
 
@@ -11,6 +8,5 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Gitrrrr!");
 	}
-
 
 }
